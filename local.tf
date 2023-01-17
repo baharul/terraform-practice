a1e7d623826dfacee1e4f73957b33dc897b809b1
bahkhan@mafcarrefour.com <bahkhan@mafcarrefour.com>
2023-01-18 01:40:29 +0400

    tested pvt key generation in terraofrm

--- local.tf
+++ local.tf
@@ -8,3 +8,13 @@ resource "random_pet" "my_pet" {
     prefix = var.prefix
     separator = var.separator
 }
+
+resource "tls_private_key" "pvtkey" {
+    algorithm="RSA"
+    rsa_bits=4096
+}
+
+resource "local_file" "key_file" {
+    filename="/Users/bahkhan/VAIPRATECH/terraform-practice/key.txt"
+    content = "${tls_private_key.pvtkey.private_key_pem}"
+}
